28ba119a942331af3ff6f9d0f93bc189a422de65
AttaDesign <162791463+AttaDesign@users.noreply.github.com>
2024-04-06 10:23:00 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,8 @@ import './globals.scss'
 import { Source_Sans_3, Orbitron } from 'next/font/google'
 import GoogleAnalytics from '@/components/GoogleAnalitics';
 import CookieBanner from '@/components/CookieBanner';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Head from 'next/head';
 
 const source = Source_Sans_3({
   subsets: ['latin'],
@@ -26,11 +27,19 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <>
+     <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="canonical" href={metadata.metadataBase.href} />
+        <meta name="robots" content="all" />
+      </Head>
     <html lang="pl">
       <GoogleAnalytics GA_MEASUREMENT_ID='G-QZB3N6PC6X' />
       <body className={`${source.variable} ${orbi.variable}`}>{children}
         <CookieBanner />
       </body>
     </html>
+  </>
   )
 }
