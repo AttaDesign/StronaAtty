f486b54294978322bbaf5f50d20464aab878a040
whit3Crrow <adam.bensari@gmail.com>
2025-04-28 17:34:16 +0200

    nowe teskty

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -10,7 +10,7 @@ function ContactSection() {
                 <div className='v'>
                     <h3 className='vh3'>Zdecydowany?</h3>
                     <p className='p font-normal'>Skontaktuj się z nami, a spersonalizujemy dla Ciebie ofertę!</p>
-                    <p className='p font-normal'>Napisz na: <Link href={"mailto:home@attadesign.pl"}><span  >home@attadesign.pl</span></Link> <br></br>lub zadzwoń na<Link href={"tel:+48531436420"}><span  >+48 531 436 420</span></Link>, <Link href={"+48729295373"}><span>+48 729 295 373</span></Link></p>
+                    <p className='p font-normal'>Napisz na: <Link href={"mailto:home@attadesign.pl"}><span  >home@attadesign.pl</span></Link> <br></br>lub zadzwoń na <Link href={"tel:+48531436420"}><span  > +48 531 436 420</span></Link>, <Link href={"+48729295373"}><span>+48 729 295 373</span></Link></p>
                 </div>
                 <Image src='/atta/aa_beztła.png' width={45} height={45} alt='logo of Bensari Solution' priority></Image>
             </div>
