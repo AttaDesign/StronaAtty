17f39d0ec344f1412dbce83d6deda0e9118cbca0
AttaDesign <162791463+AttaDesign@users.noreply.github.com>
2025-11-07 12:21:37 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+  // Dodajemy sekcję przekierowań
+  async redirects() {
+    return [
+      // Przekierowanie #1: Poprawia literówkę /en -> /eng
+      {
+        source: '/en',
+        destination: '/eng',
+        permanent: true, // 301 - stałe przekierowanie
+      },
+      // Przekierowanie #2: Poprawia stary link /cookies
+      {
+        source: '/cookies',
+        destination: '/politykaprywatnosci',
+        permanent: true, // 301 - stałe przekierowanie
+      },
+    ]
+  },
+
+}
 
 module.exports = nextConfig
