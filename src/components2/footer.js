90135cc7e33e7d62f92b67aa535bb571b10a89c8
whit3Crrow <adam.bensari@gmail.com>
2026-04-24 10:30:08 +0200

    usuniecie numerów telefonu

--- src/components2/footer.js
+++ src/components2/footer.js
@@ -7,7 +7,6 @@ export default function Footer() {
             <div className="footerbox">
                 <div className="links">
                     <h2>Contact</h2>
-                    <a href='tel:+48531436420'>+48&nbsp;531&nbsp;436&nbsp;420</a>
                     <a href='mailto:home@attadesign.pl'>home@attadesign.pl</a>
                     <Link target='blank' href='https://www.google.com/maps/place/Szlenkier%C3%B3w+2%2Fu3,+01-404+Warszawa/@52.2371761,20.9681115,17z/data=!3m1!4b1!4m5!3m4!1s0x471ecb7c72510091:0x386c1d335fbd9853!8m2!3d52.2371728!4d20.9706864?hl=pl-PL&entry=ttu'>Headquarters - Map</Link>
                     <Link href='/privacy-policy'>Privacy Policy</Link>
